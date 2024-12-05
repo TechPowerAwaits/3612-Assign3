6ceffaa1b3853c9dfe8b0db4a47f56e2697bc1ff
Richard Johnston <techpowerawaits@outlook.com>
2024-12-04 19:26:00 -0700

    Add all routes
    Just duplicated some code. Code will be simplified later.

--- api.js
+++ api.js
@@ -8,21 +8,122 @@ dataHandler.setDataRoutes(app, apiPath);
 const data = dataHandler.data;
 
 app.get(`/${apiPath}/teapot`, (req, resp) => {
-	resp.status(418).json({teapot : {short : true, stout : true}});
-})
+  resp.status(418).json({ teapot: { short: true, stout: true } });
+});
 
 app.get(`/${apiPath}/circuits/:id`, (req, resp) => {
-	const targetCircuit = data["circuits"].find(circuit => circuit.circuitId == req.params.id);
-	
-	if (targetCircuit) {
-		resp.json(targetCircuit);
-	} else {
-		declareNotFound(resp);
-	}
+  const targetCircuit = data["circuits"].find(
+    (circuit) => circuit.circuitId == req.params.id
+  );
+
+  if (targetCircuit) {
+    resp.json(targetCircuit);
+  } else {
+    declareNotFound(resp);
+  }
+});
+
+app.get(`/${apiPath}/constructors/:ref`, (req, resp) => {
+  const targetConstructor = data["constructors"].find(
+    (constructor) => constructor.constructorRef == req.params.ref
+  );
+
+  if (targetConstructor) {
+    resp.json(targetConstructor);
+  } else {
+    declareNotFound(resp);
+  }
+});
+
+app.get(`/${apiPath}/constructorResults/:ref/:year`, (req, resp) => {
+  const targetResults = data["results"].filter(
+    (result) =>
+      result.constructor.ref == req.params.ref &&
+      result.race.year == req.params.year
+  );
+
+  if (targetResults) {
+    resp.json(targetResults);
+  } else {
+    declareNotFound(resp);
+  }
+});
+
+app.get(`/${apiPath}/drivers/:ref`, (req, resp) => {
+  const targetDriver = data["drivers"].find(
+    (driver) => driver.driverRef == req.params.ref
+  );
+
+  if (targetDriver) {
+    resp.json(targetDriver);
+  } else {
+    declareNotFound(resp);
+  }
+});
+
+app.get(`/${apiPath}/driverResults/:ref/:year`, (req, resp) => {
+  const targetResults = data["results"].filter(
+    (result) =>
+      result.driver.ref == req.params.ref && result.race.year == req.params.year
+  );
+
+  if (targetResults) {
+    resp.json(targetResults);
+  } else {
+    declareNotFound(resp);
+  }
+});
+
+app.get(`/${apiPath}/races/season/:year`, (req, resp) => {
+  const targetRaces = data["races"].filter(
+    (race) => race.year == req.params.year
+  );
+
+  if (targetRaces) {
+    resp.json(targetRaces);
+  } else {
+    declareNotFound(resp);
+  }
+});
+
+app.get(`/${apiPath}/races/id/:id`, (req, resp) => {
+  const targetRace = data["races"].find((race) => race.id == req.params.id);
+
+  if (targetRace) {
+    resp.json(targetRace);
+  } else {
+    declareNotFound(resp);
+  }
+});
+
+app.get(`/${apiPath}/results/race/:id`, (req, resp) => {
+  const targetResults = data["results"].filter(
+    (result) => result.race.id == req.params.id
+  );
+
+  if (targetResults) {
+    resp.json(targetResults);
+  } else {
+    declareNotFound(resp);
+  }
+});
+
+app.get(`/${apiPath}/results/season/:year`, (req, resp) => {
+  const targetResults = data["results"].filter(
+    (result) => result.race.year == req.params.year
+  );
+
+  if (targetResults) {
+    resp.json(targetResults);
+  } else {
+    declareNotFound(resp);
+  }
 });
 
 function declareNotFound(resp) {
-	resp.status(404).json({error : {message : "Requested resource cannot be found."}});
+  resp
+    .status(404)
+    .json({ error: { message: "Requested resource cannot be found." } });
 }
 
 app.listen(3000);
--- scripts/generic-data.js
+++ scripts/generic-data.js
@@ -4,7 +4,7 @@
  * Exposes:
  * setDataRoutes -- Information from JSON files are stored and routes assigned
  * to them.
- * 
+ *
  * data -- The data acquired while setting the data routes.
  */
 
@@ -20,37 +20,48 @@ const data = {};
  * Details: The information from each file is interpreted as JSON file and is
  * inserted into data as data[baseFileName]. For example, JSON content from a
  * file `test.json` will be stored as data[test].
- * 
+ *
  * routePrefix specifies any string that will be appended to the created routes.
  * It must not end nor begin with a slash.
  */
-async function setDataRoutes(app, routePrefix = "", dataPath = defaultDataPath) {
-	const dataFiles = (await fs.readdir(dataPath)).map(filename => new DataFile(path.join(dataPath, filename)));
-	
-	await Promise.all(dataFiles.map(async dataFile => {
-		try {
-			const jsonData = await fs.readFile(dataFile.path, "utf8");
-			data[dataFile.basename] = JSON.parse(jsonData);
+async function setDataRoutes(
+  app,
+  routePrefix = "",
+  dataPath = defaultDataPath
+) {
+  const dataFiles = (await fs.readdir(dataPath)).map(
+    (filename) => new DataFile(path.join(dataPath, filename))
+  );
 
-			app.get(`/${routePrefix}${routePrefix ? "/" : ""}${dataFile.basename}`, (req, resp) => {
-				resp.json(data[dataFile.basename]);
-			});
-		} catch(error) {
-		 	console.error(`${dataFile.path}: ${error.message}`);
-		 }
-	}));
+  await Promise.all(
+    dataFiles.map(async (dataFile) => {
+      try {
+        const jsonData = await fs.readFile(dataFile.path, "utf8");
+        data[dataFile.basename] = JSON.parse(jsonData);
 
-	app.get(`/${routePrefix}`, (req, resp) => {
-		resp.json(data);
-	});
+        app.get(
+          `/${routePrefix}${routePrefix ? "/" : ""}${dataFile.basename}`,
+          (req, resp) => {
+            resp.json(data[dataFile.basename]);
+          }
+        );
+      } catch (error) {
+        console.error(`${dataFile.path}: ${error.message}`);
+      }
+    })
+  );
 
-	/*
-	 * Purpose: Creates a DataFile object.
-	 */
-	function DataFile(absPath) {
-		this.path = absPath;
-		this.basename = path.basename(absPath, path.extname(absPath));
-	}
+  app.get(`/${routePrefix}`, (req, resp) => {
+    resp.json(data);
+  });
+
+  /*
+   * Purpose: Creates a DataFile object.
+   */
+  function DataFile(absPath) {
+    this.path = absPath;
+    this.basename = path.basename(absPath, path.extname(absPath));
+  }
 }
 
-module.exports = {setDataRoutes, data};
+module.exports = { setDataRoutes, data };
